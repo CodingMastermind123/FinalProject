9fedf07ca5200704bec3a5c63a33b93c5c0687d5
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-10-05 10:32:59 -0500

    Update MovingBlock.java

--- MovingBlock.java
+++ MovingBlock.java
@@ -2,23 +2,33 @@ import mayflower.*;
 public class MovingBlock extends Block
 {
     
-
+    private static int num;
     public MovingBlock()
     {
         setImage("img/Tiles/14.png");
-        
+        num = 2;
     }
 
     public void act()
     {
         if(Mayflower.isKeyDown(Keyboard.KEY_LEFT))
         {
-            setLocation(getX() + 2, getY());
+            setLocation(getX() + num, getY());
         }
         if(Mayflower.isKeyDown(Keyboard.KEY_RIGHT))
         {
-            setLocation(getX() - 2, getY());
+            setLocation(getX() - num, getY());
         }
         super.act();
+    }
+    
+    public int getNum()
+    {
+        return num;
+    }
+    
+    public static void increaseNum()
+    {
+        num += 2;
     }
 }
