4924c4d9252be5d68026622ee7d91c325b136d1a
CodingMastermind123 <145368068+CodingMastermind123@users.noreply.github.com>
2023-10-05 10:32:24 -0500

    Update Dirt.java

--- Dirt.java
+++ Dirt.java
@@ -1,24 +1,32 @@
 import mayflower.*;
 public class Dirt extends Block
 {
-    
-
+    public static int num;
     public Dirt()
     {
         setImage("img/Tiles/5.png");
-        
+        num = 2;
     }
 
     public void act()
     {
         if(Mayflower.isKeyDown(Keyboard.KEY_LEFT))
         {
-            setLocation(getX() + 2, getY());
+            setLocation(getX() + num, getY());
         }
         if(Mayflower.isKeyDown(Keyboard.KEY_RIGHT))
         {
-            setLocation(getX() - 2, getY());
+            setLocation(getX() - num, getY());
         }
         super.act();
+    }
+    public int getNum()
+    {
+        return num;
+    }
+    
+    public static void increaseNum()
+    {
+        num += 2;
     }
 }
